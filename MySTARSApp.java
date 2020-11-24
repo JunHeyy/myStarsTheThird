37a186df46fbdbda6f1698aedff98fa7986e8ae9
JunHeyy <junjiexavier37@gmail.com>
2020-11-25 00:45:32 +0800

    Add infinite loop

--- MySTARSApp.java
+++ MySTARSApp.java
@@ -16,8 +16,8 @@ public class MySTARSApp {
        // StudentUI.display(StudentManager.getStudent("YANG0570"));
        // StudentUI.display(StudentManager.getStudent("CHAN0935"));
         boolean loginSuccess = false;
-        Staff staffjj = new Staff("Staffjj", "passjj", 2);
-        StaffManager.addStaff(staffjj);
+       // Staff staffjj = new Staff("Staffjj", "passjj", 2);
+       // StaffManager.addStaff(staffjj);
 
 
         
@@ -36,62 +36,69 @@ public class MySTARSApp {
         String accountType = null;
         String username = null;
         String password = null;
+        while(true) {
 
-        while (!loginSuccess) {
-            do {
+            while (!loginSuccess) {
+                do {
 
+                    choice = 0;
+                    while (choice != 1 && choice != 2) {
+                        try {
+                            System.out.println("Select your account type:");
+                            System.out.println("1. Student");
+                            System.out.println("2. Staff");
+                            choice = Integer.parseInt(sc.nextLine());
 
-                while (choice !=1 && choice!=2){
-                    try {
-                        System.out.println("Select your account type:");
-                        System.out.println("1. Student");
-                        System.out.println("2. Staff");
-                        choice = Integer.parseInt(sc.nextLine());
+                        } catch (Exception NumberFormatException) {
+                            System.out.println("Please enter choice again (1-2) and only integers");
+                        }
 
-                    }catch(Exception NumberFormatException) {
-                        System.out.println("Please enter choice again (1-2) and only integers");
                     }
+                    switch (choice) {
+                        case 1:
+                            accountType = "Student";
+                            //StaffController.addCourse("OODP");
+                            break;
+                        case 2:
+                            accountType = "Staff";
+                            break;
+                        default:
+                            System.out.println("Invalid input! Please select a valid choice.");
+                            break;
+                    }
+                } while (choice != 1 && choice != 2);
+                System.out.print("Enter your username: ");
+                username = sc.nextLine();
+                //char [] readin;
+               // readin = con.readPassword("Enter your password: ");
+               // password = String.valueOf(readin);
+                System.out.println("Enter your password: ");
+                password = sc.nextLine();
 
+                loginSuccess = LoginController.validate(accountType, username, password);
+
+                if (!loginSuccess) System.out.println("Username or password incorrect.");
+            }
+
+            if (accountType == "Student") {
+                Student student = StudentManager.getStudent(username);
+                LocalDateTime localDateTime = LocalDateTime.now();
+
+                if (student.getStartAccessTime().isBefore(localDateTime) && student.getEndAccessTime().isAfter(localDateTime)) {
+                    StudentUI.display(StudentManager.getStudent(username));
+                    loginSuccess = false;
                 }
-                switch (choice) {
-                    case 1:
-                        accountType = "Student";
-                        //StaffController.addCourse("OODP");
-                        break;
-                    case 2:
-                        accountType = "Staff";
-                        break;
-                    default:
-                        System.out.println("Invalid input! Please select a valid choice.");
-                        break;
-                }
-            } while (choice != 1 && choice != 2);
-            System.out.print("Enter your username: ");
-            username = sc.nextLine();
-            //char [] readin;
-           // readin = con.readPassword("Enter your password: ");
-            //password = String.valueOf(readin);
-             System.out.println("Enter your password: ");
-             password = sc.nextLine();
-
-            loginSuccess = LoginController.validate(accountType, username, password);
-
-            if (!loginSuccess) System.out.println("Username or password incorrect.");
+                else if (student.getStartAccessTime().isAfter(localDateTime))
+                    System.out.println("You are unable to access within this time period. You can only access after " + student.getStartAccessTime());
+                else if (student.getEndAccessTime().isBefore(localDateTime))
+                    System.out.println("You are unable to access within this time period. Your access time has expired. Please look for your school admin. " + student.getEndAccessTime());
+            } else if (accountType == "Staff") {
+                StaffUI.display();
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                loginSuccess = false;
+            }
         }
-        
-        if (accountType == "Student") {
-            Student student = StudentManager.getStudent(username);
-            LocalDateTime localDateTime = LocalDateTime.now();
 
-            if(student.getStartAccessTime().isBefore(localDateTime) && student.getEndAccessTime().isAfter(localDateTime))
-                StudentUI.display(StudentManager.getStudent(username));
-            else if(student.getStartAccessTime().isAfter(localDateTime))
-                System.out.println("You are unable to access within this time period. You can only access after " + student.getStartAccessTime());
-            else if (student.getEndAccessTime().isBefore(localDateTime))
-                System.out.println("You are unable to access within this time period. Your access time has expired. Please look for your school admin. " + student.getEndAccessTime());
-        }
-        else if (accountType == "Staff") {
-            StaffUI.display();
-        }
     }
 }
